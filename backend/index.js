8de227615aa26038cbad34af705979093cb22029
joser27 <joser27@uw.edu>
2025-02-18 13:47:21 -0800

    replace origin

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: [
-        'just-pong-9avjw1wwj-jose-rodriguezs-projects-ef20cd82.vercel.app',
+        'https://just-pong.vercel.app',
         'http://localhost:3000'
     ],
     methods: ['GET', 'POST'],
